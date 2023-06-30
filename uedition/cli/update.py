dbfd8bb0705ec3d46e8aa17e4d267c5c84a63b9d
Mark Hall <mark.hall@work.room3b.eu>
2023-06-30 21:51:57 +0100

    Use the new hard-coded answers filename

--- uedition/cli/update.py
+++ uedition/cli/update.py
@@ -7,4 +7,4 @@ from copier import run_update
 
 def run() -> None:
     """Update the μEdition using Copier."""
-    run_update(".", overwrite=True)
+    run_update(".", answers_file=".uEdition.answers", overwrite=True)
