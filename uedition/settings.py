90dde275a55e195c767a7c7eb78be74e394e889d
Mark <mark.hall@work.room3b.eu>
2024-01-12 13:55:50 +0000

    Dependency updates (#29)

--- uedition/settings.py
+++ uedition/settings.py
@@ -7,10 +7,59 @@ All application settings are accessed via the `settings` dictionary.
 """
 import os
 
-from pydantic import BaseSettings, BaseModel
-from pydantic.env_settings import SettingsSourceCallable
+from pydantic import BaseModel
+from pydantic.fields import FieldInfo
+from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
 from yaml import safe_load
-from typing import Any
+from typing import Any, Type, Tuple, Dict
+
+
+class YAMLConfigSettingsSource(PydanticBaseSettingsSource):
+    """Loads the configuration settings from a YAML file."""
+
+    def get_field_value(
+        self: "YAMLConfigSettingsSource", field: FieldInfo, field_name: str
+    ) -> Tuple[Any, str, bool]:
+        """Get the value of a specific field."""
+        encoding = self.config.get("env_file_encoding")
+        file_content_json = None
+        if os.path.exists("uEdition.yaml"):
+            with open("uEdition.yaml", encoding=encoding) as in_f:
+                file_content_json = safe_load(in_f)
+        elif os.path.exists("uEdition.yml"):
+            with open("uEdition.yml", encoding=encoding) as in_f:
+                file_content_json = safe_load(in_f)
+        if file_content_json is not None:
+            field_value = file_content_json.get(field_name)
+        else:
+            field_value = None
+        return field_value, field_name, False
+
+    def prepare_field_value(
+        self: "YAMLConfigSettingsSource",
+        field_name: str,
+        field: FieldInfo,
+        value: Any,
+        value_is_complex: bool,
+    ) -> Any:
+        """Just return the value."""
+        return value
+
+    def __call__(self: "YAMLConfigSettingsSource") -> Dict[str, Any]:
+        """Call the loader."""
+        d: Dict[str, Any] = {}
+
+        for field_name, field in self.settings_cls.model_fields.items():
+            field_value, field_key, value_is_complex = self.get_field_value(
+                field, field_name
+            )
+            field_value = self.prepare_field_value(
+                field_name, field, field_value, value_is_complex
+            )
+            if field_value is not None:
+                d[field_key] = field_value
+
+        return d
 
 
 def uedition_yaml_settings(settings: BaseSettings) -> dict[str, Any]:
@@ -75,26 +124,25 @@ class Settings(BaseSettings):
     jb_config: dict = {}
     """Additional JupyterBook configuration."""
 
-    class Config:
-        """Configuration overrides."""
+    @classmethod
+    def settings_customise_sources(
+        cls: Type["Settings"],
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        """Customise the settings sources."""
+        return (
+            init_settings,
+            env_settings,
+            file_secret_settings,
+            YAMLConfigSettingsSource(settings_cls),
+        )
 
-        @classmethod
-        def customise_sources(
-            cls,  # noqa: ANN102
-            init_settings: SettingsSourceCallable,
-            env_settings: SettingsSourceCallable,
-            file_secret_settings: SettingsSourceCallable,
-        ) -> tuple[SettingsSourceCallable, ...]:
-            """Add the YAMl loading functionality."""
-            return (
-                env_settings,
-                init_settings,
-                file_secret_settings,
-                uedition_yaml_settings,
-            )
 
-
-settings = Settings().dict()
+settings = Settings().model_dump()
 
 
 def reload_settings() -> None:
