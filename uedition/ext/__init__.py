ec55dc7a02714bede8104f78318af6c50bee218f
Mark <mark.hall@work.room3b.eu>
2026-05-01 16:54:38 +0200

    Return the correct Sphinx extension metadata (#96)

--- uedition/ext/__init__.py
+++ uedition/ext/__init__.py
@@ -5,11 +5,13 @@
 
 from sphinx.application import Sphinx
 
+from uedition.__about__ import __version__
 from uedition.ext import config, language_switcher, tei
 
 
-def setup(app: Sphinx) -> None:
+def setup(app: Sphinx) -> dict:
     """Run the setup process for the extensions."""
     config.setup(app)
     tei.setup(app)
     language_switcher.setup(app)
+    return {"version": __version__, "env_version": 1, "parallel_read_safe": True, "parallel_write_safe": True}
