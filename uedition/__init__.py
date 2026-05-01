68de8b8b015fa621e64061411c0633e25e6aa82a
Mark Hall <mark.hall@work.room3b.eu>
2026-05-01 16:17:08 +0100

    Load core dependencies from code

--- uedition/__init__.py
+++ uedition/__init__.py
@@ -10,4 +10,6 @@ from uedition import ext
 
 def setup(app: Sphinx) -> None:
     """Set up the theme and its extensions."""
+    for dependency in ("myst_parser", "sphinx_external_toc"):
+        app.setup_extension(dependency)
     ext.setup(app)
